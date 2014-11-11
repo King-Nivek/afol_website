f1daf8bf74510a143e5546232be2da71e827a5cb
King-Nivek <King-Nivek@users.noreply.github.com>
2014-11-11 15:32:17 -0500

    Converted to PHP files. And add a few general session tracking command.

---
+++ form_setpart.php
@@ -0,0 +1,164 @@
+<?php
+require 'resources/includes/include.global.php';
+
+  //check to see if they're logged in
+  if(!isset($_SESSION['logged_in'])) {
+    header("Location: form_sign-in.php");
+  }
+  
+  $uTool = new UserTools();
+  
+  //get the user object from the session
+  $userID = $_SESSION["userID"];
+  if ($userID == "") {
+    echo "Lost userID SESSION variable...<br>";
+    $uTool->logout();
+    header("Location: form_sign-in.php");
+  }
+  $user = $uTool->get($userID);
+  $toID = "";
+  $toUser = null;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SetPart Form</title>
+
+    <!-- Bootstrap -->
+    <link href="libraries/custom/custom-bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="resources/css/offcanvas.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+      <div class="row row-offcanvas row-offcanvas-right">
+        <div class="col-xs-12 col-sm-10">
+          
+          <div class="col-xs-10 col-sm-10 "><!--  column 1  -->
+
+            <p class="pull-right visible-xs">
+              <button type="button" class="btn btn-warning btn-xs" data-toggle="offcanvas">Toggle nav</button>
+            </p>
+
+            <div class="well">
+              <form class="form-horizontal" role="form">                      <!-- form -->
+                
+                <div class="panel panel-primary">                             <!-- Register Panel -->
+                  <div class="panel-heading">
+                    <h3 class="panel-title">Form: SetPart</h3>
+                  </div>
+                  <div class="panel-body">                                    <!-- Register Body -->
+                      <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Set ID</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="inputPassword3" placeholder="Set ID">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Color ID</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="inputPassword3" placeholder="Color ID">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Part ID</label>
+                        <div class="col-sm-10">
+                          <input type="email" class="form-control" id="inputEmail3" placeholder="Part ID">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">SetPart Quantity</label>
+                        <div class="col-sm-10">
+                          <input type="email" class="form-control" id="inputEmail3" placeholder="SetPart Quantity">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                          <button type="submit" class="btn btn-default">Submit</button>
+                        </div>
+                      </div>
+                  </div><!-- / Register Body -->
+                </div><!-- / Register Panel -->
+              </form>
+            </div>
+          </div><!--============================================================ End column 1  -->
+        
+          <div class="col-xs-2 col-sm-2 sidebar-offcanvas" id="sidebar"><!--  column 2  -->
+            <div class="well">
+              <div class="btn-group-vertical"><!--  sidebar menu items  -->
+                      
+                <!-- Main Menu -->
+                <div class="btn-group">
+                  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    Menu <span class="caret"></span>
+                  </button>
+                  <ul class="dropdown-menu dropdown-menu-right" role="menu">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="forms.php">Forms</a></li>
+                    <li><a href="reports.php">Reports</a></li>
+                    <li class="divider"></li>
+                    <li><a href="form_settings.php">Settings</a></li>
+                    <li class="divider"></li>
+                    <li><a href="logout.php">Sign-out</a></li>
+                  </ul>
+                </div>
+                
+                <!-- Page Label -->
+                <div class="h3" style="margin-top: 10px;">
+                  <span class="label label-default center-block">Forms</span>
+                </div>
+                
+                <div  style="padding-top: 10px;"></div>
+                
+                <!-- Sets -->
+                <div class="btn-group">
+                  <a class="btn btn-default" href="form_sets.php" role="button">Sets</a>
+                </div>
+                
+                <!-- Colors -->
+                <div class="btn-group">
+                  <a class="btn btn-default" href="form_parts.php" role="button">Parts</a>
+                </div>
+                
+                <!-- Parts -->
+                <div class="btn-group">
+                  <a class="btn btn-default" href="form_colors.php" role="button">Colors</a>
+                </div>
+                
+                <!-- ColorParts -->
+                <div class="btn-group">
+                  <a class="btn btn-default" href="form_colorpart.php" role="button">ColorParts</a>
+                </div>
+                
+                <!-- SetParts -->
+                <div class="btn-group">
+                  <a class="btn btn-default" href="form_setpart.php" role="button">SetParts</a>
+                </div>
+
+              </div><!--======================================================== End sidebar menu items    -->
+            </div><!--========================================================== End well                  -->
+          </div><!--============================================================ End column 2              -->
+        </div><!--============================================================== End Overall Column sizes  -->
+      </div><!--================================================================ End row                   -->
+    </div><!--================================================================== End container             -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="libraries/jquery/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="libraries/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="resources/js/offcanvas.js"></script>
+  </body>
+</html>
