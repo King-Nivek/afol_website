a9ea684d415c513915aabb50fe8fe3c24c636059
King-Nivek <King-Nivek@users.noreply.github.com>
2014-12-07 14:11:46 -0500

    Revamped to use:
      - The FormField class
      - Validations and feedback
      - The inputValid to work on the database

--- form_sets.php
+++ form_sets.php
@@ -1,6 +1,9 @@
 <?php
 require 'resources/includes/include.global.php';
 require_once 'resources/classes/class.FormField.php';
+require_once 'resources/includes/include.htmlFormButtons.php';
+require_once 'resources/php/validationTools.php';
+require_once 'resources/php/inputValid.php';
 
   //check to see if they're logged in
   if(!isset($_SESSION['logged_in'])) {
@@ -21,81 +24,155 @@ require_once 'resources/classes/class.FormField.php';
   $toUser = null;
 
   $field_setID = new FormField();
+  $field_setID->table = 'Lego_Set';
+  $field_setID->field = 'set_id';
   $field_setID->label_text = 'Set ID';
   $field_setID->id = 'setID';
   $field_setID->name = 'setID';
   $field_setID->placeholder = '70702-1';
+  $field_setID->maxlength = 10;
   $field_setID->tooltip_text = 'Please enter the Lego Set number.';
 
   $field_setName = new FormField();
+  $field_setName->table = 'Lego_Set';
+  $field_setName->field = 'set_name';
   $field_setName->label_text = 'Set Name';
   $field_setName->id = 'setName';
   $field_setName->name = 'setName';
   $field_setName->placeholder = 'Warp Stinger';
+  $field_setName->maxlength = 50;
   $field_setName->tooltip_text = 'Please enter the Lego Set name.';
 
   $field_setTheme = new FormField();
+  $field_setTheme->table = 'Lego_Set';
+  $field_setTheme->field = 'set_category';
   $field_setTheme->label_text = 'Set Theme';
   $field_setTheme->id = 'setTheme';
   $field_setTheme->name = 'setTheme';
   $field_setTheme->placeholder = 'Galaxy Squad';
+  $field_setTheme->maxlength = 40;
   $field_setTheme->tooltip_text = 'Please enter the Lego Set Theme name.';
   
-  // print_r($_POST['keys']);
+  $formButtons = '';
+  $submitType = 'false';
+  $validInput = 0;
+  $originalKeys = array();
+
+  $keys = array();
+  $keys[] = &$field_setID;
+
   if($_SERVER["REQUEST_METHOD"] == "POST") {
-    // echo "in server<br>\n";
-    if(isset($_POST['keys'])) {
-      // echo "in post<br>\n";
+
+    if(isset($_SESSION['originalKeys'])) {
+      $originalKeys = unserialize($_SESSION['originalKeys']);
+    }
+
+    if(isset($_POST['submitType'])) {
+      $submitType = makeSafer($_POST['submitType']);
+      if(!($submitType === 'modify' || $submitType === 'delete' || $submitType === 'addNew')) {
+        $submitType = 'false';
+      }
+    }
+
+    if(isset($submitType) && $submitType === 'addNew') {
+      $formButtons = formAddRecord();
+    } else {
+      $formButtons = formModifyDeleteRecord();
+    }
+
+    $input = array();
+
+    if(isset($_POST[$field_setID->id ])) {                      //  Check if $_POST['setID'] is set.
+      if(!empty($_POST[$field_setID->id ])) {                   //  Check if it is empty.
+        $input[$field_setID->id ] = makeSafer($_POST[$field_setID->id ]); //  Trim, strip slashes, and make certain character into their entity form.
+        $field_setID->value = $input[$field_setID->id ];        //  Set the fields value.
+
+        if(isSetID($input[$field_setID->id ])) {  //  Check that the input matches allowed characters and or character types.
+          $field_setID->has_success();  //  Shows check mark if valid.
+          $validInput++;
+        } else {
+          $field_setID->error = 'Bad Value';  //  Error String is set.
+          $field_setID->has_error();          //  Shows and 'X' that it is not valid.
+        }
+      } else {
+        $field_setID->has_required();  //  Shows an asterisk if the field was required and submitted empty.
+      }
+    }
+
+    if(isset($_POST[$field_setName->id])) {                        //  Check if $_POST['setName'] is set.
+      if(!empty($_POST[$field_setName->id])) {                     //  Check if it is empty.
+        $input[$field_setName->id] = makeSafer($_POST[$field_setName->id]); //  Trim, strip slashes, and make certain character into their entity form.
+        $field_setName->value = $input[$field_setName->id];        //  Set the fields value.
+
+        if(isSetName($input[$field_setName->id])) {  //  Check that the input matches allowed characters and or character types.
+          $field_setName->has_success();    //  Shows check mark if valid.
+          $validInput++;
+        } else {
+          $field_setName->error = 'Bad Value';  //  Error String is set.
+          $field_setName->has_error();          //  Shows and 'X' that it is not valid.
+        }
+      } else {
+        $field_setName->has_required();  //  Shows an asterisk if the field was required and submitted empty.
+      }
+    }
+
+    if(isset($_POST[$field_setTheme->id])) {                         //  Check if $_POST['setTheme'] is set.
+      if(!empty($_POST[$field_setTheme->id])) {                      //  Check if it is empty.
+        $input[$field_setTheme->id] = makeSafer($_POST[$field_setTheme->id]); //  Trim, strip slashes, and make certain character into their entity form.
+        $field_setTheme->value = $input[$field_setTheme->id];        //  Set the fields value.
+
+        if(isSetName($input[$field_setTheme->id])) { //  Check that the input matches allowed characters and or character types.
+          $field_setTheme->has_success();   //  Shows check mark if valid.
+          $validInput++;
+        } else {
+          $field_setTheme->error = 'Bad Value'; //  Error String is set.
+          $field_setTheme->has_error();                 //  Shows and 'X' that it is not valid.
+        }
+      } else {
+        $field_setTheme->has_required(); //  Shows an asterisk if the field was required and submitted empty.
+      }
+    }
+
+    $data = array_merge($field_setID->get_column_Value(),
+                        $field_setName->get_column_Value(),
+                        $field_setTheme->get_column_Value());
+
+    if($submitType === 'delete') {
+      whatToDo($submitType, $keys, $data, $originalKeys);
+    }
+
+    if($validInput === 3 && $submitType !== 'delete') {
+      whatToDo($submitType, $keys, $data, $originalKeys);
+    }
+
+
+    if(isset($_POST['keys']) && !empty($_POST['keys'])) {
+
       $posted_keys = $_POST['keys'];
       $posted_keys = substr_replace($posted_keys,"}",-2);
       $posted_keys = str_replace("'",'"',$posted_keys);
       $posted_keys = json_decode($posted_keys);
-      // print_r($posted_keys);
+
       $results = $db->select("Lego_Set","*","set_id='$posted_keys->set_id'");
-      // print_r($results);
+      
+      $originalKeys = array();
+      $originalKeys[] = array('field' => 'set_id', 'value' => $results['set_id']);
+
+      $_SESSION['originalKeys'] = serialize($originalKeys);
+
       $field_setID->value = $results['set_id'];
       $field_setName->value = $results['set_name'];
       $field_setTheme->value = $results['set_category'];
-
     }
+
+  } else {
+      $formButtons = formAddRecord();
   }
 
+echo html_header("Set form"),
+     html_containerStart() ;
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Set Form</title>
-
-    <!-- Bootstrap -->
-    <link href="libraries/custom/custom-bootstrap.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="resources/css/offcanvas.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <div class="container">
-      <div class="row row-offcanvas row-offcanvas-right">
-        <div class="col-xs-12 col-sm-10">
-          
-          <div class="col-xs-10 col-sm-10 "><!--  column 1  -->
-
-            <p class="pull-right visible-xs">
-              <button type="button" class="btn btn-warning btn-xs" data-toggle="offcanvas">Toggle nav</button>
-            </p>
-
-            <div class="well">
-              <form class="form-horizontal" role="form">
+              <form id="theForm" class="form-horizontal" role="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
                 
                 <div class="panel panel-primary">
 
@@ -105,50 +182,19 @@ require_once 'resources/classes/class.FormField.php';
 
                   <div class="panel-body">
                     <?php 
-                      echo $field_setID->printField(),"\n",
-                           $field_setName->printField(),"\n",
-                           $field_setTheme->printField(),"\n"; 
+                      echo $field_setID->toString(),"\n",
+                           $field_setName->toString(),"\n",
+                           $field_setTheme->toString(),"\n",
+                           $formButtons,"\n";
                     ?>
-                    <div class="form-group">
-                      <div class="col-sm-offset-3 col-sm-9">
-                        <button type="button"
-                                name="submit"
-                                value="modify" 
-                                class="btn btn-default"
-                        >Modify</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <button type="button"
-                                name="submit"
-                                value="delete" 
-                                class="btn btn-danger"
-                        >Delete</button>
-                      </div>
-                    </div>
+                    
                   </div>
                 </div><!-- End panel  -->
-              </form>
-            </div>
-          </div><!--============================================================ End column 1  -->
-        
-          <div class="col-xs-2 col-sm-2 sidebar-offcanvas" id="sidebar"><!--  column 2  -->
-            <div class="well">
-              <div class="btn-group-vertical"><!--  sidebar menu items  -->
-                      
-                <!-- Main Menu -->
-                <div class="btn-group">
-                  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                    Menu <span class="caret"></span>
-                  </button>
-                  <ul class="dropdown-menu dropdown-menu-right" role="menu">
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="forms.php">Forms</a></li>
-                    <li><a href="reports.php">Reports</a></li>
-                    <li class="divider"></li>
-                    <li><a href="form_settings.php">Settings</a></li>
-                    <li class="divider"></li>
-                    <li><a href="logout.php">Sign-out</a></li>
-                  </ul>
-                </div>
                 
+              </form>
+<?php echo html_column1End(),
+           html_menuStart();
+?>              
                 <!-- Page Label -->
                 <div class="h3" style="margin-top: 10px;">
                   <span class="label label-default center-block">Forms</span>
@@ -156,43 +202,15 @@ require_once 'resources/classes/class.FormField.php';
                 
                 <div  style="padding-top: 10px;"></div>
                 
-                <!-- Sets -->
+                <!-- Return to Forms page -->
                 <div class="btn-group">
                   <a class="btn btn-default" href="forms.php" role="button">Forms Home</a>
                 </div>
-                <!-- 
-                <!-- Colors - ->
-                <div class="btn-group">
-                  <a class="btn btn-default" href="form_parts.php" role="button">Parts</a>
-                </div>
-                
-                <!-- Parts - ->
-                <div class="btn-group">
-                  <a class="btn btn-default" href="form_colors.php" role="button">Colors</a>
-                </div>
-                
-                <!-- ColorParts - ->
-                <div class="btn-group">
-                  <a class="btn btn-default" href="form_colorpart.php" role="button">ColorParts</a>
-                </div>
-                
-                <!-- SetParts - ->
-                <div class="btn-group">
-                  <a class="btn btn-default" href="form_setpart.php" role="button">SetParts</a>
-                </div>
- -->
-              </div><!--======================================================== End sidebar menu items    -->
-            </div><!--========================================================== End well                  -->
-          </div><!--============================================================ End column 2              -->
-        </div><!--============================================================== End Overall Column sizes  -->
-      </div><!--================================================================ End row                   -->
-    </div><!--================================================================== End container             -->
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="libraries/jquery/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="libraries/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="resources/js/offcanvas.js"></script>
+<?php 
+  echo html_Ending(),"\n",
+       formDeleteScripts();
+?>
     <script>
       $(function () { 
         $("[data-toggle='tooltip']").tooltip(); 
